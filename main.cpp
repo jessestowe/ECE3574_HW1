3614a5fb86436eef8c4518b20acc09b16d3e5fc6
Jesse Stowe <jstowe@localhost.localdomain>
2016-01-30 13:52:28 -0500

    add in includes and a build script

--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
 #include <QCoreApplication>
+#include <QDate>
+#include <QFile>
+#include <QString>
+#include <QStringList>
+#include <QTextStream>
+
+#define DATAFILE "birthdays.dat"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    return a.exec();
+    //Setup in/out streams
+    QTextStream cout(stdout);
+    QTextStream err(stderr);
+
+    //Open birthday file
+    QFile dataFile(DATAFILE);
+    QTextStream infile(&dataFile);
+    QTextStream outfile(&dataFile);
+    QString line = infile.readLine();
+
+    while (!line.isNull()) {
+        process_line(line);
+        line = infile.readLine();
+    }
+
+    //return a.exec();
+    return EXIT_SUCCESS;
+
 }
+
+
